e5943aacbfbadce0e700d86f3c665436a7889ed2
osutton2010 <osutton2010@gmail.com>
2020-07-30 19:55:41 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ImageProcessorTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.buttonPickColour = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.AnyColor = true;
+            this.colorDialog.SolidColorOnly = true;
+            // 
+            // buttonPickColour
+            // 
+            this.buttonPickColour.Location = new System.Drawing.Point(12, 130);
+            this.buttonPickColour.Name = "buttonPickColour";
+            this.buttonPickColour.Size = new System.Drawing.Size(142, 23);
+            this.buttonPickColour.TabIndex = 0;
+            this.buttonPickColour.Text = "Pick Colour";
+            this.buttonPickColour.UseVisualStyleBackColor = true;
+            this.buttonPickColour.Click += new System.EventHandler(this.ButtonPickColour_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(142, 112);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 165);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonPickColour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button buttonPickColour;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using ImageProcessor;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageProcessorTest
+{
+  public partial class Form1 : Form
+  {
+    private readonly string filePath = "C:\\Users\\Ollie\\source\\repos\\ImageProcessor\\sqaures.png";
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void ButtonPickColour_Click(object sender, EventArgs e)
+    {
+      var result = colorDialog.ShowDialog();
+      
+      if (result == DialogResult.OK)
+      {
+        var selectedColour = colorDialog.Color;
+
+        // Do the image changing here.
+        byte[] photoBytes = File.ReadAllBytes(filePath);
+        using (MemoryStream inStream = new MemoryStream(photoBytes))
+        using (MemoryStream outStream = new MemoryStream())
+        using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+        {
+          imageFactory.Load(inStream).ReplaceColor(Color.Black, selectedColour).Save(outStream);
+
+          pictureBox.Image = Image.FromStream(outStream);
+        }
+
+      }
+    }
+  }
+}
